f494d6fe0d53215585d3525d2cc82f6e805c1f06
tangshihao <tang_shihao@icekredit.com>
2019-09-18 11:52:02 +0800

    fix: 提交1

--- springboot/src/test/java/org/tangshihao/study/springboot/dao/DataDimensionMapperTest.java
+++ springboot/src/test/java/org/tangshihao/study/springboot/dao/DataDimensionMapperTest.java
@@ -23,6 +23,6 @@ public class DataDimensionMapperTest {
     @Test
     public void test() {
         System.out.println("yes");
-        System.out.println("no");
+        System.out.println("n");
     }
 }
